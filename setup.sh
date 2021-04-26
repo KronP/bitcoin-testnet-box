cfe19974298e77af864e224d8cc49eafb7d4674e
Patrik Kron <17673053+KronP@users.noreply.github.com>
2021-04-26 14:39:36 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# cd bitcoin-testnet-box
+mkdir tmp
+cd tmp
+export BITCOIN_CORE_VERSION="0.21.0"
+wget "https://bitcoincore.org/bin/bitcoin-core-${BITCOIN_CORE_VERSION}/bitcoin-${BITCOIN_CORE_VERSION}-x86_64-linux-gnu.tar.gz"
+tar xzf "bitcoin-${BITCOIN_CORE_VERSION}-x86_64-linux-gnu.tar.gz"
+cd "bitcoin-${BITCOIN_CORE_VERSION}/bin"
+install --mode 755 --target-directory /usr/local/bin *
+
+cd /workspace/bitcoin-testnet-box/
+rm -r tmp
+make start
+
+bitcoin-cli -rpcport=19001 -rpcpassword=123 -rpcuser=admin1 createwallet testwallet
+
+make generate BLOCKS=200
